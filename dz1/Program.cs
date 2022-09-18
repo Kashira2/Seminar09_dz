4224e5efa1a02178bd49d65e2f85734cf1329ec3
Kashira2 <nekit.egorov@icloud.com>
2022-09-18 12:37:24 +0300

    dz2

---
+++ dz1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Введите первое число M: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число N: ");
+int num2 = int.Parse(Console.ReadLine());
+
+string NumberFor (int num2, int num1)
+{
+    if (num2 == num1) return $"{num1}";
+    else return NumberFor(num2 - 1, num1) + $" {num2}";
+}
+
+Console.WriteLine(NumberFor(num2, num1));
---
+++ dz2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumRec(int num2, int num1)
+{
+    if (num2 == num1) return num1;
+    else return num2 + SumRec(num2 - 1, num1);
+}
+
+try
+{
+Console.WriteLine("Введите первое число M: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число N: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(SumRec(num2, num1));
+}
+catch
+{
+    Console.WriteLine("Вводите целочисленные значения");
+}
