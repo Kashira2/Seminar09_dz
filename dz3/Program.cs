075cafa19232cce099d21aa24c897c84b987ea57
Kashira2 <nekit.egorov@icloud.com>
2022-09-20 13:43:24 +0300

    дфые

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Функциия Аккермана для двух неотрицательных чисел.
+
+int Akkerman(int n, int m)
+{
+    int result = 0;
+    if (n == 0)
+    {
+        result = m + 1;
+        return result;
+    }
+    else if (n != 0 && m == 0)
+    {
+        return Akkerman(n - 1, 1);
+    }
+    else return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+try
+{
+    Console.Write("Введите первое положительное число число n: ");
+    int n = int.Parse(Console.ReadLine());
+    Console.Write("Введите второе положительное число m: ");
+    int m = int.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+    Console.WriteLine(Akkerman(n, m));
+}
+catch
+{
+    Console.WriteLine("Вводите целочисленные значения");
+}
